19e1b51f69980c3473d267322711b9474959354c
LuizzFellipe <luiz.fellipe@aluno.ifsp.edu.br>
2023-08-10 17:50:21 -0300

    Criação do Home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Header from "./Header";
+import Home from "./Home";
 
 const App = () => {
 
     return (
-        <Header/>
+        <Home/>
     );
 };
 
---
+++ src/Home.jsx
@@ -0,0 +1,18 @@
+import './Home.css';
+
+const Home = () => {
+  return (
+    <header>
+        <h1>Liverpool</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </nav>
+    </header>
+  );
+};
+
+export default Home
