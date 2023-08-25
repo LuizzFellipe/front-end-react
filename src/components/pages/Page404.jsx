9fe660cb00771eef3ee708e7a7f6dcbdd0a4caf4
LuizzFellipe <luiz.fellipe@aluno.ifsp.edu.br>
2023-08-25 14:56:13 -0300

    Novas Paginas

---
+++ src/components/pages/Page404.jsx
@@ -0,0 +1,16 @@
+import Headers from '../layout/Headers';
+import Footer from '../layout/Footer';
+
+import React from 'react'
+
+const Page404 = () => {
+  return (
+    <>
+        <Headers />
+            <h1>Página não Encontrada</h1>
+        <Footer />
+    </>
+  )
+}
+
+export default Page404
---
+++ src/components/pages/QuemSomos.jsx
@@ -0,0 +1,22 @@
+import Headers from '../layout/Headers';
+import Sidebar from '../layout/Sidebar';
+import Content from '../layout/Content';
+import Footer from '../layout/Footer';
+
+import React from 'react'
+
+const QuemSomos = () => {
+  return (
+    <>
+        <Headers/>
+        <div id='main'>
+            <Sidebar/>
+            <h1>Quem Somos</h1>
+            <Content/>
+        </div>
+        <Footer/>
+    </>
+  )
+}
+
+export default QuemSomos
